8ae6fcd40b2f5947523aa250c891a0cd6cd01d03
kento13410 <107041136+kento13410@users.noreply.github.com>
2022-09-06 07:48:32 +0000

    Tue, Sep 6, 2022, 7:48 AM +00:00

--- app.py
+++ app.py
@@ -1,16 +1,54 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from cs50 import SQL
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.chrome.options import Options
+import re
+import chromedriver_binary
 
 app = Flask("__name__")
-db = SQL("sqlite:///foodname.db")
 
-url = db.execute("SELECT * FROM foodnames")[11]["画像"]
-print(url)
 
 @app.route("/")
 def index():
- return render_template("home.html", url=url)
+    return render_template("input.html")
 
 
+@app.route("/search_item")
+def search_item():
+    option = Options()
+    option.add_argument('--headless')
 
+    browser = webdriver.Chrome(executable_path=r"C:\Users\kento\Downloads\chromedriver_win32\chromedriver.exe", options=option)
 
+    try:
+        url = 'https://fooddb.mext.go.jp/freeword/fword_top.pl'
+        browser.get(url)
+
+        browser.switch_to.frame(browser.find_element_by_tag_name("iframe"))
+
+        keywordBox = browser.find_element_by_class_name('s-text')
+        search = browser.find_element_by_name('function1')
+
+        keyword = request.form.get("breakfast")
+
+        keywordBox.send_keys(keyword)
+        search.click()
+
+        browser.find_element_by_class_name('result_button').click()
+
+        foods = []
+        valueNames = browser.find_elements_by_css_selector('#result_table > tbody > tr')
+        for valueName in valueNames:
+            items = valueName.find_elements_by_css_selector('td')
+            for i in range(len(items)):
+                foods.append(items[0].text)
+
+            # print(f'{items[0].text}, エネルギー：{items[2].text}kcal, たんぱく質：{items[4].text}g, 脂質：{items[5].text}g,
+            # 炭水化物：{items[6].text}g （100g当たり）')
+
+    finally:
+        # プラウザを閉じる
+        browser.quit()
+
+    return render_template("input.html", foods=foods)
