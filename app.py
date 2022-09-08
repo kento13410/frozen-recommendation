62e931a0f14d84c3b3511b2121d3d7a48f652499
kento13410 <kento13410@gmail.com>
2022-09-08 00:35:09 +0000

    Thu, Sep 8, 2022, 12:35 AM +00:00

--- app.py
+++ app.py
@@ -118,16 +118,17 @@ def index():
 # -------------------------------------------------------上がテスター------------------------------------------------------------------
 
 
-# @app.route("/search_item")
-# def search_item():
-#     breakfast = request.form.get("breakfast")
-#     lunch = request.form.get("lunch")
-#     snack = request.form.get("snack")
-#     brName = db.execute("SELECT 食品名 FROM 食品成分 WHERE 食品名 like %?%", breakfast)
-#     luName = db.execute("SELECT 食品名 FROM 食品成分 WHERE 食品名 like %?%", lunch)
-#     snName = db.execute("SELECT 食品名 FROM 食品成分 WHERE 食品名 like %?%", snack)
-#     return render_template("select.html", breakfast=brName, lunch=luName, snack=snName)
-#     food_energy = db.execute("SELECT エネルギー FROM 食品成分 WHERE 食品名 like %?% OR 食品名 like %?% OR 食品名 like %?%", breakfast, lunch, snack)
+@app.route("/search_item")
+def search_item():
+    breakfast = request.form.get("breakfast")
+    lunch = request.form.get("lunch")
+    snack = request.form.get("snack")
+    sql1 = "SELECT 食品名 FROM 食品成分 WHERE 食品名 like ?"
+    brName = db.execute(sql1, breakfast+"%")
+    luName = db.execute("SELECT 食品名 FROM 食品成分 WHERE 食品名 like %?%", lunch)
+    snName = db.execute("SELECT 食品名 FROM 食品成分 WHERE 食品名 like %?%", snack)
+    return render_template("select.html", breakfast=brName, lunch=luName, snack=snName)
+    # food_energy = db.execute("SELECT エネルギー FROM 食品成分 WHERE 食品名 like %?% OR 食品名 like %?% OR 食品名 like %?%", breakfast, lunch, snack)
 
 
 @app.route("/recommend", methods=["GET","POST"])
