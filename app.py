3f628291b0898147ca4912bbd3562b1be18cd0a5
kento13410 <107041136+kento13410@users.noreply.github.com>
2022-09-10 09:41:37 +0000

    Sat, Sep 10, 2022, 9:41 AM +00:00

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def index():
         difData = {'カロリー': D, 'タンパク質': difP, '脂質': difF, '炭水化物': difCBH}
 
         # カロリー*0.7< act< カロリー*1.3
-        data = db.execute("SELECT * FROM foodnames WHERE カロリー*0.9 < ? < カロリー*1.1 AND タンパク質*0.7 < ? < タンパク質*1.3 AND 脂質*0.7 < ? < 脂質*1.3 AND 炭水化物*0.7 < ? < 炭水化物*1.3", act, difP, difF, difCBH)
+        data = db.execute("SELECT * FROM foodnames WHERE カロリー*0.9 < ? < カロリー*1.1 AND タンパク質*0.7 < ? < タンパク質*1.3 AND 脂質*0.7 < ? < 脂質*1.3 AND 炭水化物*0.7 < ? < 炭水化物*1.3", D, difP, difF, difCBH)
         # data = db.execute("SELECT * FROM foodnames WHERE カロリー < ?", D)
 
         return render_template("output_tester.html", data = data, difData=difData)
