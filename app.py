9e8e3c6c208ada5d861a03676de385261d801faf
kento13410 <107041136+kento13410@users.noreply.github.com>
2022-09-15 14:32:01 +0000

    Thu, Sep 15, 2022, 2:32 PM +00:00

--- app.py
+++ app.py
@@ -129,7 +129,7 @@ def index():
 
     else:
         if session:
-            personal_data = db.execute("SELECT * FROM personal_data WHERE user_id = ?", session['user_id'])[0]
+            personal_data = db1.execute("SELECT * FROM personal_data WHERE user_id = ?", session['user_id'])[0]
             act = act_calculate(personal_data['sex'], personal_data['weight'], personal_data['height'], personal_data['age'], personal_data['level'], personal_data['activity'])
 
         else:
