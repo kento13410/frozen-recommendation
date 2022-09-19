fea8deb7bbe6b6dcfa7cc72341d2d42beb4c7808
kento13410 <107041136+kento13410@users.noreply.github.com>
2022-09-19 10:32:04 +0000

    Mon, Sep 19, 2022, 10:32 AM +00:00

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def login():
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
-        return render_template("login.html")
+        return render_template("main/login.html")
 # ---------------------------------------------------------------------------------------------------------------
 
 # --------------------------------------登録画面(register)---------------------------------------------------
