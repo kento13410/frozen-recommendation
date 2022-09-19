9bdfabb98d17ab81d7c4342fd636768d5344f711
kento13410 <107041136+kento13410@users.noreply.github.com>
2022-09-19 04:04:21 +0000

    Mon, Sep 19, 2022, 4:04 AM +00:00

--- app.py
+++ app.py
@@ -288,9 +288,9 @@ def recommend():
 @app.route("/personal_data", methods=['GET', 'POST'])
 def personal_data():
     if request.method == 'POST':
-        age = int(request.form.get("age2"))
-        weight = int(request.form.get("weight2"))
-        height = int(request.form.get("height2"))
+        age = int(request.form.get("age"))
+        weight = int(request.form.get("weight"))
+        height = int(request.form.get("height"))
         sex = request.form.get("sex")
         try:
             # 目標
